c77e5d4d6206403c86c4e23e965265cf8ff44062
Ceci Mermoz <ceciliamermoz@cliengo.com>
2021-01-12 15:20:48 -0300

    ItemCount.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemList from "./components/ItemList/ItemList";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer greetings="Hola Bo!"/>
+      <ItemListContainer greetings="¡Saludos!"/>
+      <ItemList />
+
     </div>
   );
 }
