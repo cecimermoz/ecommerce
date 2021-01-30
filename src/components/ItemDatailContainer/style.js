ab0868487475f7d4cfa4f26735b79615ea80bb2f
Ceci Mermoz <ceciliamermoz@cliengo.com>
2021-01-30 13:41:21 -0300

    add spinner and loader

---
+++ src/components/ItemDatailContainer/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const LoaderGif = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-basis:100%;
+    margin-top: 15%;
+`;
+
+export default LoaderGif;
